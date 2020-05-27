8eb9e2073d551256616a807c6d2c06dcb8f3f5ac
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-27 15:58:43 +0300

    Setup simple socket communication

---
+++ graph_test.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io')
+
+const app = express();
+
+const server = app.listen(3000);
+
+const io = socket(server);
+
+app.use(express.static('./client'));
+
+// io.on('connection', socket => {
+//     setInterval(() => {
+//         socket.emit('asdf', 'hello');
+//     }, 1000);
+// });
+
+setInterval(() => {
+    io.sockets.emit('asdf', 'hello');
+}, 1000);
