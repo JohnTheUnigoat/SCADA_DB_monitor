b043fe45331f98c008fd75c1561c0cba6d12a6f5
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-28 15:59:28 +0300

    Add loading new data from db

--- server.js
+++ server.js
@@ -42,12 +42,38 @@ var scadaVarValues = (function() {
 })();
 
 
-// Load initial variable values, send first message
+// Load initial variable values, save latest timestamps
 conn.query(initVarValuesSql, (err, res) => {
     if (err) throw err;
 
     saveData(res);
+
+    setInterval(() => {
+        loadNewData();
+    }, 1000);
 });
+
+
+function loadNewData() {
+    let condition = "";
+
+    scadaVarIds.forEach(id => {
+        let varData = scadaVarValues[id];
+        condition += `(ID = ${id} AND Timestamp > '${varData.lastTimestamp}') OR `;
+    });
+
+    condition = condition.slice(0, condition.lastIndexOf(' OR'));
+
+    let sql = `SELECT ID, Timestamp, Value FROM trends_data WHERE ${condition}`;
+
+    conn.query(sql, (err, res) => {
+        if (err) throw err;
+
+        console.log(`${res.length} records loaded`);
+
+        saveData(res);
+    });
+}
 
 function saveData(queryRes) {
     if (queryRes.length == 0) return;
@@ -62,11 +88,10 @@ function saveData(queryRes) {
     for (const id in scadaVarValues) {
         let varData = scadaVarValues[id];
         varData.lastTimestamp = varData.records[varData.records.length - 1].time;
+
         console.log(id);
         varData.records.forEach(record => {
             console.log(record);
         });
     }
-
-    console.log(scadaVarValues);
 }
