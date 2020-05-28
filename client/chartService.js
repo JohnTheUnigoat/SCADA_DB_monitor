c2edea5153b993e38facfff72f2551183b56a486
Ayrum1158 <ayrum1158@gmail.com>
2020-05-28 20:58:17 +0300

    Changed x axis to time, optimised code, uninstalled webpack, webpack-cli
    Signed-off-by: Ayrum1158 <ayrum1158@gmail.com>

--- client/chartService.js
+++ client/chartService.js
@@ -3,52 +3,47 @@ var id_chartDict = {};
 var id_label = {44: "one", 45: "two", 46: "three"}
 function createCharts()// for all canvases
 {
-    let canvases = Array.prototype.slice.call(document.getElementsByTagName("canvas"));// turn to js array
-    canvases.forEach(canvas => {
-        console.log("I am alive!");
-        let ctx = document.getElementById(canvas.id).getContext('2d');
+    let canvases = document.getElementsByTagName("canvas");
+    for (let canvas of canvases) {
+        let ctx = canvas.getContext('2d');
         let newChart = new Chart(ctx, {
             type: 'line',
             data: {
                 datasets: [{
                     lineTension: 0,
-                    showLine: true,
-                    spanGaps: true,
-                    label: '',
                     borderColor: 'rgb(255, 0, 0)',
+                    data: []
                 }]
             },
             options: {
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    enabled: false
+                },
                 animation: false,
                 scales: {
                     xAxes: [{
-                        type: 'linear',
-                        display: true,
+                        type: 'time',
                     }],
-                    yAxes: [{
-                        display: true,
-                    }]
                 }
             }
         });
 
         newChart.data.datasets[0].label = id_label[canvas.id];
-        console.log("I am alive!!!!!!!!!!!!!!!");
         id_chartDict[canvas.id] = newChart;
-    });
+    }
 }
 createCharts();
 
 function addValue(message)
 {
     let id = message.id;
-    let date = new Date(message.time);
-    // let time = date.customFormat("#hhhh#:#mm#:#ss#");
-    // console.log(time);
-    let xval = date;
+    let xval = moment(message.time);
     let yval = message.value;
 
-    id_chartDict[id].data.datasets[0].data.push({x: xval, y: yval});
+    id_chartDict[id].data.datasets[0].data.push({t: xval, y: yval});
 
     id_chartDict[id].update();
 }
